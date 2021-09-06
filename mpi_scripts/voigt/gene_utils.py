6eaa3b1deaba9d2e0ec0b7471a5f882b6c846e58
Andrey Pikunov <pikunov@phystech.edu>
2021-09-06 17:37:25 +0300

    bug fix: is_multiplier for non-common genes

--- mpi_scripts/voigt/gene_utils.py
+++ mpi_scripts/voigt/gene_utils.py
@@ -1,11 +1,13 @@
 def create_genes_dict_from_config(config):
-    gamma_default = config.get('gamma', 1)
+    gamma_default = config.get("gamma", 1)
 
     genes_dict = {
         ec_name: {
             p_name: dict(
                 bounds=p["bounds"],
-                gamma=(gamma_default * p["gamma_multiplier"]) if "gamma_multiplier" in p else None,
+                gamma=(gamma_default * p["gamma_multiplier"])
+                if "gamma_multiplier" in p
+                else None,
                 is_multiplier=p.get("is_multiplier", False),
             )
             for p_name, p in ec["params"].items()
@@ -31,8 +33,10 @@ def create_constants_dict_from_config(config):
 
 
 def generate_bounds_gammas_mask_multipliers(genes_dict):
-    bounds, gammas, mask_multipliers = [[x[item] for y in genes_dict.values() for x in y.values()]
-                                        for item in ["bounds", "gamma", "is_multiplier"]]
+    bounds, gammas, mask_multipliers = [
+        [x[item] for y in genes_dict.values() for x in y.values()]
+        for item in ["bounds", "gamma", "is_multiplier"]
+    ]
     return bounds, gammas, mask_multipliers
 
 
@@ -45,29 +49,34 @@ def update_S_C_from_genes(S, C, genes, exp_cond_name, config):
         **constants_dict[exp_cond_name],
     }
 
-    for i, g_name in enumerate(genes.index.get_level_values("g_name")):
+    idx_frame = genes.index.to_frame(index=False)
 
-        if g_name in legend["constants"].index:
-            for ecn in ["common", exp_cond_name]:
-                if g_name in genes_dict[ecn]:
-                    if genes_dict[ecn][g_name]["is_multiplier"]:
-                        C[g_name] *= genes[ecn, g_name]
-                    else:
-                        C[g_name] = genes[ecn, g_name]
+    for (_, idx), value in zip(idx_frame.iterrows(), genes.values):
+        ec_name, g_name = idx["ec_name"], idx["g_name"]
 
-        if g_name in legend["states"].index:
-            for ecn in ["common", exp_cond_name]:
-                if g_name in genes_dict[ecn]:
-                    if genes_dict[ecn][g_name]["is_multiplier"]:
-                        S[g_name] *= genes[ecn, g_name]
-                    else:
-                        S[g_name] = genes[ecn, g_name]
+        if ec_name in ["common", exp_cond_name]:
+
+            if g_name in legend["constants"]:
+                df = C
+            elif g_name in legend["states"]:
+                df = S
+            else:
+                msg = "No such gene in legends: {g_name}".format(g_name=g_name)
+                raise KeyError(msg)
+
+            if genes_dict[ec_name][g_name]["is_multiplier"]:
+                df[g_name] *= genes[ec_name, g_name]
+            else:
+                df[g_name] = genes[ec_name, g_name]
 
     for c_name, c in constants_dict_current.items():
-        if c_name in legend["constants"].index:
+        if c_name in legend["constants"]:
             C[c_name] = c
-        if c_name in legend["states"].index:
+        elif c_name in legend["states"]:
             S[c_name] = c
+        else:
+            msg = "No such constant in legends: {c_name}".format(c_name=c_name)
+            raise KeyError(msg)
 
 
 def update_genes_from_state(genes, state, config, exp_cond_name):
@@ -81,7 +90,7 @@ def update_genes_from_state(genes, state, config, exp_cond_name):
                 if g_name in genes_dict[ecn]:
                     if genes_dict[ecn][g_name]["is_multiplier"]:
                         genes[ecn, g_name] = (
-                                state[exp_cond_name][g_name] / legend["states"][g_name]
+                            state[exp_cond_name][g_name] / legend["states"][g_name]
                         )
                     else:
                         genes[ecn, g_name] = state[exp_cond_name][g_name]
